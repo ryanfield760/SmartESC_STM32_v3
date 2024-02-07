d92d74a9e80d736925ba7530763acefcecbb7b3f
ryanfield760 <159285527+ryanfield760@users.noreply.github.com>
2024-02-08 02:54:45 +1100

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -4,21 +4,21 @@
 #define BRAKEMAX 100
 
 // speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
-#define SPEEDLIMIT_SPORT 50
+#define SPEEDLIMIT_ECO 60
+#define SPEEDLIMIT_NORMAL 60
+#define SPEEDLIMIT_SPORT 60
 
 // motor current limits for invividual modes in mA
 // note that hacked firmware allows up to 55amps motor phase current
-#define PH_CURRENT_MAX_ECO 10000
-#define PH_CURRENT_MAX_NORMAL 15000
-#define PH_CURRENT_MAX_SPORT 25000
+#define PH_CURRENT_MAX_ECO 27000
+#define PH_CURRENT_MAX_NORMAL 30000
+#define PH_CURRENT_MAX_SPORT 40000
 
 // motor current limit for regen in mA
 #define REGEN_MAX_CURRENT 10000
 
 // maximum current for field weakening in mA
-#define FIELD_WEAKNING_CURRENT_MAX 0 //max id
+#define FIELD_WEAKNING_CURRENT_MAX 25000 //max id
 
 // ADC channels
 #define ADC_VOLTAGE 0
